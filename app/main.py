65a923e7a7f247af3727619bde7737ce413836e5
Andrei Shiffer <andreishiffer@Andreis-MacBook-Air.local>
2020-02-08 16:35:33 -0500

    u

--- app/main.py
+++ app/main.py
@@ -102,10 +102,10 @@ def move():
        #     direction = 'left'
        # if last_move == 'left':
        #     direction = 'up'
+    last_move = direction
   #cant kill itself does not work for now!!!!
     if last_move == '':
         direction = 'up'
-
     if last_move == 'down':
         direction != 'up'
     if last_move == 'up':
@@ -187,7 +187,7 @@ def move():
             direction = 'right'
            
         
-    last_move = direction
+
 
     
  
